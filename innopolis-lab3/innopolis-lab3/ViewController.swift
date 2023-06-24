2731d12364cecc21e8a3c98784af1ab3f96b81d3
Snapman7 <renat_ja02@mail.ru>
2023-06-24 18:25:33 +0300

    First commit

---
+++ innopolis-lab3/innopolis-lab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  innopolis-lab3
+//
+//  Created by Ренат Хайруллин on 24.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
